aec05f25d3f17836de7c6226d07fd5d2d2c083cf
Yannick Mauray <yannick.mauray@gmail.com>
2025-06-11 15:39:55 +0200

    feat: sample settings

---
+++ settings-sample.ts
@@ -0,0 +1,8 @@
+import { Settings } from './src/settings';
+
+export const settings: Settings = {
+    source: 'sources/mybook/My Book.docx',
+    output: 'output.docx',
+    authors: ["Alice Smith", "Bob Johnson", "Charlie Brown"],
+    title: "My Book Title",
+};
