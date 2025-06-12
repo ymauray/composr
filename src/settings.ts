011a19c5553111f208d69e62c756945fb4f4e9b3
Yannick Mauray <yannick.mauray@gmail.com>
2025-06-12 15:36:59 +0200

    doc: Adds licensing
    Includes a standard header with copyright information and
    GNU GPLv3 licensing details to all source files.

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,18 @@
+// Définition de la classe Settings pour la configuration principale du projet imposr.
+// Copyright (C) 2025 Yannick Mauray
+//
+// Ce programme est un logiciel libre : vous pouvez le redistribuer et/ou le modifier
+// selon les termes de la Licence Publique Générale GNU publiée par la Free Software Foundation,
+// soit la version 3 de la licence, soit (à votre gré) toute version ultérieure.
+//
+// Ce programme est distribué dans l'espoir qu'il sera utile,
+// mais SANS AUCUNE GARANTIE : sans même la garantie implicite de
+// QUALITÉ MARCHANDE ou D'ADÉQUATION À UN USAGE PARTICULIER. Voir la
+// Licence Publique Générale GNU pour plus de détails.
+//
+// Vous devriez avoir reçu une copie de la Licence Publique Générale GNU
+// avec ce programme. Si ce n'est pas le cas, voir <https://www.gnu.org/licenses/>.
+
 export class Settings {
     source: string = '';
     cover: string = '';
