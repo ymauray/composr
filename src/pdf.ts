72a146a46c83580b9d405683a1ad84da5ed351af
Yannick Mauray <yannick.mauray@gmail.com>
2025-06-13 10:39:01 +0200

    doc: adds GPLv3 license header
    Adds a GPLv3 license header to the file, clarifying the terms of use and distribution for the software.
    
    This ensures compliance with open-source licensing standards and informs users of their rights and obligations regarding the code.

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,3 +1,18 @@
+// Génération d'un fichiers PDF à partir d'un fichier DOCX'.
+// Copyright (C) 2025 Yannick Mauray
+//
+// Ce programme est un logiciel libre : vous pouvez le redistribuer et/ou le modifier
+// selon les termes de la Licence Publique Générale GNU publiée par la Free Software Foundation,
+// soit la version 3 de la licence, soit (à votre gré) toute version ultérieure.
+//
+// Ce programme est distribué dans l'espoir qu'il sera utile,
+// mais SANS AUCUNE GARANTIE : sans même la garantie implicite de
+// QUALITÉ MARCHANDE ou D'ADÉQUATION À UN USAGE PARTICULIER. Voir la
+// Licence Publique Générale GNU pour plus de détails.
+//
+// Vous devriez avoir reçu une copie de la Licence Publique Générale GNU
+// avec ce programme. Si ce n'est pas le cas, voir <https://www.gnu.org/licenses/>.
+
 import { exec } from "child_process";
 
 export async function convertToPdf(inputPath: string): Promise<string> {
